5c6f47ec7abbd087d1c10520bb600384ddec98d5
Gabriel Azevedo Alvarenga <gazevedoalvarenga@gmail.com>
2024-11-07 23:29:12 -0300

    .

--- heap.java
+++ heap.java
@@ -1,1 +1,18 @@
+class heap{
+    public static void main(String args[]){
+        //array[i] é a raiz
+        //array[i/2] é pai do array[i]
+        // se array[2*i] e array[2*i +1] existem eles sao pais do array[i]
+        //se i é maior que n/2, i é uma folha
+    }
+}
 
+
+
+
+/*
+ * Exercicio resolvido 4
+ * void metodo(int i){
+ *  
+ * }
+ */
