7b88ed1182d30c95c912a5612994469aa04f4503
Gabriel Azevedo Alvarenga <gazevedoalvarenga@gmail.com>
2024-11-18 19:34:52 -0300

    .

--- ArvoreBinaria/introducao.java
+++ ArvoreBinaria/introducao.java
@@ -21,7 +21,7 @@ class No{
 class ArvoreBinaria{
     No raiz;
     ArvoreBinaria() { raiz = null;}
-
+  // Inserção com retorno de referencia
     void Inserir(int x) throws Exception{
         raiz = Inserir(x, raiz);
     }
@@ -37,5 +37,23 @@ class ArvoreBinaria{
             throw new Exception("erro!");
         }
         return i;
+    }
+
+    //inserção com passagem de pai
+
+    void InserirPai(int x) throws Exception{
+        if(raiz == null){
+            raiz = new No(x);
+        } else if(x< raiz.elemento){
+            InserirPai(x, raiz.esq, raiz);
+        } else if(x > raiz.elemento){
+            InserirPai(x, raiz.dir, raiz);
+        }else{
+            throw new Exception("Erro");
+        }
+    }
+
+    void InserirPai(int x, No i, No pai){
+        
     }
 }
